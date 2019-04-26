a7534a581f5971046c67103f1677e9d871d1396b
Todor Stoyanov <t.i.stoianov@abv.bg>
2019-04-26 23:46:49 +0300

    fix

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -11,7 +11,7 @@ export default class Calculator extends Component {
             equation: '',
             result: 0,
             resultStore: [],
-            index: 0,
+            index: -1,
             counterMaxLenght: 0
         }
     };
@@ -55,22 +55,29 @@ export default class Calculator extends Component {
         console.log()
         this.setState({ equation: '', result: 0 })
     }
+
     forward = () => {
         let index = this.state.index;
-        index++;
-        if (this.state.resultStore.length < index) {
+        console.log(index)
+        console.log(this.state.resultStore)
+        if (index >= this.state.resultStore.length ) {
             return;
         }
         const resultObj = this.state.resultStore[index];
-        this.setState({ result: resultObj.result, equation: resultObj.equation,index:index })
-    }   
+        index++;
+        this.setState({ result: resultObj.result, equation: resultObj.equation, index: index })
+    }
+
     backward = () => {
         let index = this.state.index;
-        index--;
-        if (index < 0) {
+        
+        console.log(index)
+        console.log(this.state.resultStore)
+        if (index <= 0) {
             return;
         }
         const resultObj = this.state.resultStore[index];
+        index--;
         this.setState({ result: resultObj.result, equation: resultObj.equation, index: index })
     }
 
