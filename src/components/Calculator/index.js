05b8ccd04060e9f4c221b59b206604d75c6e7302
Todor Stoyanov <t.i.stoianov@abv.bg>
2019-04-27 00:27:40 +0300

    code formatting

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -12,22 +12,23 @@ export default class Calculator extends Component {
             result: 0,
             resultStore: [],
             index: -1,
-            counterMaxLength: 0
+            counterMaxLength: 0,
+            isContainDot: false
         }
     };
 
     calculateResult = () => {
 
-        let result = math.eval(this.state.equation)
+        let result = math.eval(this.state.equation);
         let resultStore = this.state.resultStore;
         let resultObj = {
             result: result,
             equation: this.state.equation
-        }
-        resultStore.push(resultObj)
+        };
+        resultStore.push(resultObj);
         let index = this.state.index;
         index++;
-        return this.setState({ equation: '', result: result, resultStore: resultStore, counterMaxLength: 0, index: index })
+        return this.setState({ equation: '', result: result, resultStore: resultStore, counterMaxLength: 0, index: index });
 
     }
 
@@ -37,45 +38,51 @@ export default class Calculator extends Component {
 
         let equation = this.state.equation;
         if ((target >= 0 && target <= 9) || target === '.') {
+
             if (this.state.counterMaxLength >= 9) {
                 return;
             }
-            this.setState({ counterMaxLength: this.state.counterMaxLength + 1 })
+            if (target === '.') {
+                if (this.state.isContainDot) { return };
+                this.setState({ isContainDot: true });
+            }
+            this.setState({ counterMaxLength: this.state.counterMaxLength + 1 });
             equation += target;
 
         } else if (target === '+' || target === '-' || target === '*' || target === '/' || target === '%') {
-            this.setState({ counterMaxLength: 0 })
+            this.setState({ isContainDot: false });
+            this.setState({ counterMaxLength: 0 });
             equation += ' ' + target + ' ';
 
         }
-        this.setState({ equation: equation })
+        this.setState({ equation: equation });
     }
 
     clear = () => {
         console.log()
-        this.setState({ equation: '', result: 0 })
+        this.setState({ equation: '', result: 0 });
     }
 
     forward = () => {
         let index = this.state.index;
-       
-        
-        
-        if (index < this.state.resultStore.length-1 ) {
+
+
+
+        if (index < this.state.resultStore.length - 1) {
             index++;
         }
         const resultObj = this.state.resultStore[index];
-        this.setState({ result: resultObj.result, equation: resultObj.equation, index: index })
+        this.setState({ result: resultObj.result, equation: resultObj.equation, index: index });
     }
 
     backward = () => {
         let index = this.state.index;
-       
+
         if (index > 0) {
             index--;
         }
         const resultObj = this.state.resultStore[index];
-        this.setState({ result: resultObj.result, equation: resultObj.equation, index: index })
+        this.setState({ result: resultObj.result, equation: resultObj.equation, index: index });
     }
 
     render() {
